94ec67ae6cdded4aeb4b4d5c341e81c5ff1412d2
Zachary Sterman <zacharysterman1992@gmail.com>
2021-10-08 12:02:12 -0400

    most updated file

--- process.py
+++ process.py
@@ -4,9 +4,9 @@ log_file = open("um-server-01.txt") #opening the information needed to reference
 def sales_reports(log_file): # defining sales reprts with our inforamtion listed above
     for line in log_file: #looping over all files in log_file
         line = line.rstrip() #stripping file of line end inforamtion included in .txt 
-        day = line[0:3] #referencing particular line information (quantity)
+        day = line[0:3] #referencing particular line information (day)
         if day == "Mon": #defining we are looking for monday info
             print(line) # printing what we just asked for
 
 
-sales_reports(log_file) #showing info in terminal
+sales_reports(log_file) #showing data in terminal
